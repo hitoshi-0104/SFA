9b1190b7d334025b00bcb8a4aeb8718735b13673
Y <y.hitoshi0104@gmail.com>
2017-11-06 15:42:34 +0900

    Add LeadDao, LeadEntity

---
+++ src/dao/LeadDao.java
@@ -0,0 +1,19 @@
+package dao;
+
+import entity.LeadEntity;
+
+/**
+ * 見込み客Dao
+ *
+ */
+public class LeadDao {
+
+	/**
+	 * 新規登録
+	 * @param entity
+	 * @return
+	 */
+	public boolean insert(LeadEntity entity) {
+		return true;
+	}
+}
