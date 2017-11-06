a88ffec8a46711da30765d3a59c37e424f79d732
Hitoshi Yamazaki <hitoshi@hitoshi-0104>
2017-11-06 15:24:07 +0900

    Change validate method for Lead Create View

--- src/service/lead/create/RegistService.java
+++ src/service/lead/create/RegistService.java
@@ -33,71 +33,75 @@ public class RegistService {
 				new ValidationType[] { ValidationType.MAXLENGTH },
 				messageMap);
 		// 会社名：必須入力・最大桁数チェック
-		
-		
-//		// 会社名
-//		if (Validater.isEmpty(dto.getCompanyName())) {
-//			// 必須入力
-//			messageMap.put("E00100401", String.format(MessageReader.read("E001"), ItemDefine.COMPANY_NAME));
-//		} else if (Validater.isOverflow(dto.getCompanyName(), 200)) {
-//			// 200桁オーバー
-//			messageMap.put("E00100402", String.format(MessageReader.read("E002"), ItemDefine.COMPANY_NAME, 200));
-//		}
-//		// 役職名
-//		if (Validater.isOverflow(dto.getPosition(), 50)) {
-//			// 50桁オーバー
-//			messageMap.put("E00100501", String.format(MessageReader.read("E002"), ItemDefine.POSITION, 50));
-//		}
-//		// 状況
-//		if (Validater.isEmpty(dto.getStatusCode())) {
-//			// 必須入力
-//			messageMap.put("E00100701", String.format(MessageReader.read("E001"), ItemDefine.STATUS));
-//		}
-//		// 電話
-//		if (Validater.isNotNumericOnly(dto.getPhone())) {
-//			// 数値以外
-//			messageMap.put("E00100901", String.format(MessageReader.read("E004"), ItemDefine.PHONE));
-//		} else if (Validater.isOverflow(dto.getPhone(), 11)) {
-//			// 11桁オーバー
-//			messageMap.put("E00100902", String.format(MessageReader.read("E002"), ItemDefine.PHONE, 11));
-//		}
-//		// 携帯
-//		if (Validater.isNotNumericOnly(dto.getMobilePhone())) {
-//			// 数値以外
-//			messageMap.put("E00101001", String.format(MessageReader.read("E004"), ItemDefine.MOBILE_PHONE));
-//		} else if (Validater.isOverflow(dto.getMobilePhone(), 11)) {
-//			// 11桁オーバー
-//			messageMap.put("E00101002", String.format(MessageReader.read("E002"), ItemDefine.MOBILE_PHONE, 11));
-//		}
-//		// FAX
-//		if (Validater.isNotNumericOnly(dto.getFax())) {
-//			// 数値以外
-//			messageMap.put("E00101001", String.format(MessageReader.read("E004"), ItemDefine.FAX));
-//		} else if (Validater.isOverflow(dto.getFax(), 11)) {
-//			// 11桁オーバー
-//			messageMap.put("E00101002", String.format(MessageReader.read("E002"), ItemDefine.FAX, 11));
-//		}
-//		// メール
-//		if (Validater.isNotMailAddressFormat(dto.getMailAddress())) {
-//			// メールアドレスフォーマット以外
-//			messageMap.put("E00101201", String.format(MessageReader.read("E007"), ItemDefine.MAILADDRESS));
-//		} else if (Validater.isOverflow(dto.getMailAddress(), 256)) {
-//			// 256桁オーバー
-//			messageMap.put("E00101202", String.format(MessageReader.read("E002"), ItemDefine.MAILADDRESS, 256));
-//		}
-//		// URL
-//		if (Validater.isNotUrlFormat(dto.getUrl())) {
-//			// メールアドレスフォーマット以外
-//			messageMap.put("E00101301", String.format(MessageReader.read("E009"), ItemDefine.URL));
-//		} else if (Validater.isOverflow(dto.getUrl(), 2048)) {
-//			// 2048桁オーバー
-//			messageMap.put("E00101302", String.format(MessageReader.read("E002"), ItemDefine.URL, 2048));
-//		}
-//		// 年間売上
-//		if (Validater.isOverflow(String.valueOf(dto.getAmount()), 13)) {
-//			// 13桁オーバー
-//			messageMap.put("E00101302", String.format(MessageReader.read("E002"), ItemDefine.URL, 2048));
-//		}
+		Validater.validate(dto.getCompanyName(), "E001004", ItemDefine.Name.COMPANY_NAME,
+				null, ItemDefine.Digit.COMPANY_NAME,
+				new ValidationType[] { ValidationType.REQURIRED, ValidationType.MAXLENGTH },
+				messageMap);
+		// 役職名
+		Validater.validate(dto.getPosition(), "E001005", ItemDefine.Name.POSITION,
+				null, ItemDefine.Digit.POSITION,
+				new ValidationType[] { ValidationType.MAXLENGTH },
+				messageMap);
+		// 状況
+		Validater.validate(dto.getStatusCode(), "E001007", ItemDefine.Name.STATUS,
+				null, null,
+				new ValidationType[] { ValidationType.REQURIRED },
+				messageMap);
+		// 電話
+		Validater.validate(dto.getPhone(), "E001009", ItemDefine.Name.PHONE,
+				null, ItemDefine.Digit.PHONE,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.NUMERICONLY },
+				messageMap);
+		// 携帯
+		Validater.validate(dto.getMobilePhone(), "E001010", ItemDefine.Name.MOBILE_PHONE,
+				null, ItemDefine.Digit.MOBILE_PHONE,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.NUMERICONLY },
+				messageMap);
+		// FAX
+		Validater.validate(dto.getFax(), "E001011", ItemDefine.Name.FAX,
+				null, ItemDefine.Digit.FAX,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.NUMERICONLY },
+				messageMap);
+		// メール
+		Validater.validate(dto.getMailAddress(), "E001012", ItemDefine.Name.MAILADDRESS,
+				null, ItemDefine.Digit.MAILADDRESS,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.MAIL_FORMAT },
+				messageMap);
+		// URL
+		Validater.validate(dto.getUrl(), "E001013", ItemDefine.Name.URL,
+				null, ItemDefine.Digit.URL,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.URL_FORMAT },
+				messageMap);
+		// 年間売上
+		Validater.validate(dto.getAmount(), "E001014", ItemDefine.Name.AMOUNT,
+				null, ItemDefine.Digit.AMOUNT,
+				new ValidationType[] { ValidationType.MAXLENGTH },
+				messageMap);
+		// 従業員数
+		Validater.validate(dto.getEmployees(), "E001016", ItemDefine.Name.EMPLOYEE,
+				null, ItemDefine.Digit.EMPLOYEE,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.NUMERICONLY },
+				messageMap);
+		// 郵便番号
+		Validater.validate(dto.getPostalCode(), "E001017", ItemDefine.Name.POSTAL_CODE,
+				null, ItemDefine.Digit.POSTAL_CODE,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.NUMERICONLY },
+				messageMap);
+		// 市区町村
+		Validater.validate(dto.getCity(), "E001019", ItemDefine.Name.CITY,
+				null, ItemDefine.Digit.CITY,
+				new ValidationType[] { ValidationType.MAXLENGTH },
+				messageMap);
+		// 町名・番地・建物名
+		Validater.validate(dto.getCity(), "E001020", ItemDefine.Name.TOWN,
+				null, ItemDefine.Digit.TOWN,
+				new ValidationType[] { ValidationType.MAXLENGTH },
+				messageMap);
+		// その他
+		Validater.validate(dto.getCity(), "E001021", ItemDefine.Name.NOTE,
+				null, ItemDefine.Digit.NOTE,
+				new ValidationType[] { ValidationType.MAXLENGTH },
+				messageMap);
 
 		return messageMap;
 	}
