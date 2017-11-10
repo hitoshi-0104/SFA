1015c1240f85c540fd01865e7885a542916b1ea9
Y <y.hitoshi0104@gmail.com>
2017-11-10 17:05:55 +0900

    LeadDao.insertのテストを修正

--- test/src/dao/LeadDaoTest.java
+++ test/src/dao/LeadDaoTest.java
@@ -8,6 +8,7 @@ import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.Test;
 
 import dao.entity.LeadEntity;
+import dao.entity.SequenceEntity;
 
 /**
  * LeadDaoクラスUnitテスト
@@ -20,6 +21,19 @@ class LeadDaoTest {
 	 */
 	@Test
 	void testInsert() {
+
+		// シーケンスの取得
+		SequenceDao sDao = new SequenceDao();
+		SequenceEntity se = null;
+		try {
+			se = sDao.selectByName("T_LEAD");
+		} catch (Exception e) {
+			fail(e.getMessage());
+			return;
+		}
+		Integer id = se.getSeq() + 1;
+
+		// insertするデータ作成
 		LeadEntity entity = new LeadEntity();
 
 		// 姓
@@ -63,11 +77,11 @@ class LeadDaoTest {
 		// その他
 		entity.setNote("０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９０１２３４５６７８９");
 		// 作成者ID
-		entity.setCreaterId("0123456789");
+		entity.setCreaterId(123456789);
 		// 作成日時
 		entity.setCreateDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss.SSS")));
 		// 更新者ID
-		entity.setUpdaterId("1234567890");
+		entity.setUpdaterId(234567890);
 		// 更新日時
 		entity.setUpdateDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss.SSS")));
 
@@ -79,7 +93,65 @@ class LeadDaoTest {
 			return;
 		}
 
+		// チェック
+		LeadEntity chk = null;
+		try {
+			chk = dao.selectById(id);
+		} catch (Exception e) {
+			fail(e.getMessage());
+			return;
+		}
 
+		// ID
+		assertEquals(id, chk.getId());
+		// 姓
+		assertEquals(entity.getLastName(), chk.getLastName());
+		// 名
+		assertEquals(entity.getFirstName(), chk.getFirstName());
+		// 会社名
+		assertEquals(entity.getCompanyName(), chk.getCompanyName());
+		// 役職名
+		assertEquals(entity.getPosition(), chk.getPosition());
+		// ソース
+		assertEquals(entity.getSourceCode(), chk.getSourceCode());
+		// 状況
+		assertEquals(entity.getStatusCode(), chk.getStatusCode());
+		// 評価
+		assertEquals(entity.getEstimationCode(), chk.getEstimationCode());
+		// 電話
+		assertEquals(entity.getPhone(), chk.getPhone());
+		// 携帯
+		assertEquals(entity.getMobilePhone(), chk.getMobilePhone());
+		// FAX
+		assertEquals(entity.getFax(), chk.getFax());
+		// メール
+		assertEquals(entity.getMailAddress(), chk.getMailAddress());
+		// URL
+		assertEquals(entity.getUrl(), chk.getUrl());
+		// 業種
+		assertEquals(entity.getIndustryCode(), chk.getIndustryCode());
+		// 年間売上
+		assertEquals(entity.getAmount(), chk.getAmount());
+		// 従業員数
+		assertEquals(entity.getEmployees(), chk.getEmployees());
+		// 郵便番号
+		assertEquals(entity.getPostalCode(), chk.getPostalCode());
+		// 都道府県
+		assertEquals(entity.getDivisionCode(), chk.getDivisionCode());
+		// 市区郡
+		assertEquals(entity.getCity(), chk.getCity());
+		// 町名・番地・建物名
+		assertEquals(entity.getTown(), chk.getTown());
+		// その他
+		assertEquals(entity.getNote(), chk.getNote());
+		// 作成者ID
+		assertEquals(entity.getCreaterId(), chk.getCreaterId());
+		// 作成日時
+		assertEquals(entity.getCreateDate(), chk.getCreateDate());
+		// 更新者ID
+		assertEquals(entity.getUpdaterId(), chk.getUpdaterId());
+		// 更新日時
+		assertEquals(entity.getUpdateDate(), chk.getUpdateDate());
 	}
 
 }
