c0f332aaa2b9198989ff2336973ca3432c60fa4e
Y <y.hitoshi0104@gmail.com>
2017-11-22 14:25:22 +0900

    Json作成クラスの新規作成

---
+++ src/util/json/JsonProvider.java
@@ -0,0 +1,30 @@
+package util.json;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * JSON作成クラス
+ *
+ */
+public class JsonProvider {
+
+	/**
+	 * MapからJSON作成
+	 * @param m
+	 * @return
+	 */
+	public static String provide(Map<String, String> m) {
+		return "";
+	}
+
+	/**
+	 * MapのListからJSON作成
+	 * @param list
+	 * @return
+	 */
+	public static String provide(List<Map<String, String>> list) {
+		return "";
+	}
+
+}
