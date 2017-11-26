727d7c566b60f88ab3e4cfb5d7cd045592e1a838
Y <y.hitoshi0104@gmail.com>
2017-11-26 16:56:37 +0900

    Rest APIのコンテンツタイプのcharsetを指定なしからUTF-8に変更

--- src/util/constant/ServletSettings.java
+++ src/util/constant/ServletSettings.java
@@ -9,7 +9,7 @@ public class ServletSettings {
 	/** コンテンツタイプ */
 	public static final String CONTENT_TYPE = "text/html; charset=UTF-8";
 	/** Restのコンテンツタイプ */
-	public static final String REST_CONTENT_TYPE = "application/json";
+	public static final String REST_CONTENT_TYPE = "application/json; charset=UTF-8";
 	/** エンコード */
 	public static final String CHARACTER_ENCODING = "UTF-8";
 	/** URLアトリビュート名 */
