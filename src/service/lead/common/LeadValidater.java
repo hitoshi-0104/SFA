880824b88f53a881f5e3aba235d043eeed60de6c
Y <y.hitoshi0104@gmail.com>
2017-12-02 16:39:32 +0900

    見込み客機能共通の入力チェック処理を実装

---
+++ src/service/lead/common/LeadValidater.java
@@ -0,0 +1,112 @@
+package service.lead.common;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import service.common.Validater;
+import service.common.ValidationType;
+import service.lead.dto.LeadDto;
+import util.constant.ItemDefine;
+
+/**
+ * 見込み客機能用の入力チェッククラス
+ *
+ */
+public class LeadValidater {
+
+	/**
+	 * バリデート
+	 * @param dto
+	 * @return
+	 */
+	public Map<String, String> validate(LeadDto dto, String displayId) {
+
+		Map<String, String> messageMap = new LinkedHashMap<String, String>();
+		String id = "E" + displayId;
+
+		// 姓：必須入力・最大桁数チェック
+		Validater.validate(dto.getLastName(), id + "002", ItemDefine.Name.LAST_NAME,
+				null, ItemDefine.Digit.LAST_NAME,
+				new ValidationType[] { ValidationType.REQURIRED, ValidationType.MAXLENGTH },
+				messageMap);
+		// 名：最大桁数チェック
+		Validater.validate(dto.getFirstName(), id + "003", ItemDefine.Name.FIRST_NAME,
+				null, ItemDefine.Digit.FIRST_NAME,
+				new ValidationType[] { ValidationType.MAXLENGTH },
+				messageMap);
+		// 会社名：必須入力・最大桁数チェック
+		Validater.validate(dto.getCompanyName(), id + "004", ItemDefine.Name.COMPANY_NAME,
+				null, ItemDefine.Digit.COMPANY_NAME,
+				new ValidationType[] { ValidationType.REQURIRED, ValidationType.MAXLENGTH },
+				messageMap);
+		// 役職名：最大桁数チェック
+		Validater.validate(dto.getPosition(), id + "005", ItemDefine.Name.POSITION,
+				null, ItemDefine.Digit.POSITION,
+				new ValidationType[] { ValidationType.MAXLENGTH },
+				messageMap);
+		// 状況：必須入力チェック
+		Validater.validate(dto.getStatusCode(), id + "007", ItemDefine.Name.STATUS,
+				null, null,
+				new ValidationType[] { ValidationType.REQURIRED },
+				messageMap);
+		// 電話：最大桁数・数値のみチェック
+		Validater.validate(dto.getPhone(), id + "009", ItemDefine.Name.PHONE,
+				null, ItemDefine.Digit.PHONE,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.NUMERICONLY },
+				messageMap);
+		// 携帯：最大桁数・数値のみチェック
+		Validater.validate(dto.getMobilePhone(), id + "010", ItemDefine.Name.MOBILE_PHONE,
+				null, ItemDefine.Digit.MOBILE_PHONE,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.NUMERICONLY },
+				messageMap);
+		// FAX：最大桁数・数値のみチェック
+		Validater.validate(dto.getFax(), id + "011", ItemDefine.Name.FAX,
+				null, ItemDefine.Digit.FAX,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.NUMERICONLY },
+				messageMap);
+		// メール：最大桁数・メールアドレスフォーマットチェック
+		Validater.validate(dto.getMailAddress(), id + "012", ItemDefine.Name.MAILADDRESS,
+				null, ItemDefine.Digit.MAILADDRESS,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.MAIL_FORMAT },
+				messageMap);
+		// URL：最大桁数・URLフォーマットチェック
+		Validater.validate(dto.getUrl(), id + "013", ItemDefine.Name.URL,
+				null, ItemDefine.Digit.URL,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.URL_FORMAT },
+				messageMap);
+		// 年間売上：最大桁数・数値のみチェック
+		Validater.validate(dto.getAmount(), id + "014", ItemDefine.Name.AMOUNT,
+				null, ItemDefine.Digit.AMOUNT,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.NUMERICONLY },
+				messageMap);
+		// 従業員数：最大桁数・数値のみチェック
+		Validater.validate(dto.getEmployees(), id + "016", ItemDefine.Name.EMPLOYEE,
+				null, ItemDefine.Digit.EMPLOYEE,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.NUMERICONLY },
+				messageMap);
+		// 郵便番号：最大桁数・数値のみチェック
+		Validater.validate(dto.getPostalCode(), id + "017", ItemDefine.Name.POSTAL_CODE,
+				null, ItemDefine.Digit.POSTAL_CODE,
+				new ValidationType[] { ValidationType.MAXLENGTH, ValidationType.NUMERICONLY },
+				messageMap);
+		// 市区町村：最大桁数チェック
+		Validater.validate(dto.getCity(), id + "019", ItemDefine.Name.CITY,
+				null, ItemDefine.Digit.CITY,
+				new ValidationType[] { ValidationType.MAXLENGTH },
+				messageMap);
+		// 町名・番地・建物名：最大桁数チェック
+		Validater.validate(dto.getCity(), id + "020", ItemDefine.Name.TOWN,
+				null, ItemDefine.Digit.TOWN,
+				new ValidationType[] { ValidationType.MAXLENGTH },
+				messageMap);
+		// その他：最大桁数チェック
+		Validater.validate(dto.getCity(), id + "021", ItemDefine.Name.NOTE,
+				null, ItemDefine.Digit.NOTE,
+				new ValidationType[] { ValidationType.MAXLENGTH },
+				messageMap);
+
+		return messageMap;
+
+	}
+
+}
---
+++ src/service/lead/dto/LeadDto.java
@@ -0,0 +1,305 @@
+package service.lead.dto;
+
+/**
+ * 見込み客のDtoクラス
+ *
+ */
+public class LeadDto {
+
+	/** ID */
+	private Integer id;
+	/** 姓 */
+	private String lastName;
+	/** 名 */
+	private String firstName;
+	/** 会社名 */
+	private String companyName;
+	/** 役職名 */
+	private String position;
+	/** ソース */
+	private String sourceCode;
+	/** 状況 */
+	private String statusCode;
+	/** 評価 */
+	private String estimationCode;
+	/** 電話 */
+	private String phone;
+	/** 携帯 */
+	private String mobilePhone;
+	/** FAX */
+	private String fax;
+	/** メール */
+	private String mailAddress;
+	/** URL */
+	private String url;
+	/** 業種 */
+	private String industryCode;
+	/** 売上 */
+	private Long amount;
+	/** 従業員数 */
+	private Integer employees;
+	/** 郵便番号 */
+	private String postalCode;
+	/** 都道府県 */
+	private String divisionCode;
+	/** 市区郡 */
+	private String city;
+	/** 町名・番地・建物 */
+	private String town;
+	/** その他 */
+	private String note;
+
+	/**
+	 * @return id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id セットする id
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName セットする lastName
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName セットする firstName
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return companyName
+	 */
+	public String getCompanyName() {
+		return companyName;
+	}
+	/**
+	 * @param companyName セットする companyName
+	 */
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	/**
+	 * @return position
+	 */
+	public String getPosition() {
+		return position;
+	}
+	/**
+	 * @param position セットする position
+	 */
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	/**
+	 * @return sourceCode
+	 */
+	public String getSourceCode() {
+		return sourceCode;
+	}
+	/**
+	 * @param sourceCode セットする sourceCode
+	 */
+	public void setSourceCode(String sourceCode) {
+		this.sourceCode = sourceCode;
+	}
+	/**
+	 * @return statusCode
+	 */
+	public String getStatusCode() {
+		return statusCode;
+	}
+	/**
+	 * @param statusCode セットする statusCode
+	 */
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+	/**
+	 * @return estimationCode
+	 */
+	public String getEstimationCode() {
+		return estimationCode;
+	}
+	/**
+	 * @param estimationCode セットする estimationCode
+	 */
+	public void setEstimationCode(String estimationCode) {
+		this.estimationCode = estimationCode;
+	}
+	/**
+	 * @return phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+	/**
+	 * @param phone セットする phone
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	/**
+	 * @return mobilePhone
+	 */
+	public String getMobilePhone() {
+		return mobilePhone;
+	}
+	/**
+	 * @param mobilePhone セットする mobilePhone
+	 */
+	public void setMobilePhone(String mobilePhone) {
+		this.mobilePhone = mobilePhone;
+	}
+	/**
+	 * @return fax
+	 */
+	public String getFax() {
+		return fax;
+	}
+	/**
+	 * @param fax セットする fax
+	 */
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+	/**
+	 * @return mailAddress
+	 */
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	/**
+	 * @param mailAddress セットする mailAddress
+	 */
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	/**
+	 * @return url
+	 */
+	public String getUrl() {
+		return url;
+	}
+	/**
+	 * @param url セットする url
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	/**
+	 * @return industry
+	 */
+	public String getIndustryCode() {
+		return industryCode;
+	}
+	/**
+	 * @param industryCode セットする industryCode
+	 */
+	public void setIndustryCode(String industryCode) {
+		this.industryCode = industryCode;
+	}
+	/**
+	 * @return amount
+	 */
+	public Long getAmount() {
+		return amount;
+	}
+	/**
+	 * @param amount セットする amount
+	 */
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
+	/**
+	 * @return employees
+	 */
+	public Integer getEmployees() {
+		return employees;
+	}
+	/**
+	 * @param employees セットする employees
+	 */
+	public void setEmployees(Integer employees) {
+		this.employees = employees;
+	}
+	/**
+	 * @return postalCode
+	 */
+	public String getPostalCode() {
+		return postalCode;
+	}
+	/**
+	 * @param postalCode セットする postalCode
+	 */
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	/**
+	 * @return division
+	 */
+	public String getDivisionCode() {
+		return divisionCode;
+	}
+	/**
+	 * @param divisionCode セットする divisionCode
+	 */
+	public void setDivisionCode(String divisionCode) {
+		this.divisionCode = divisionCode;
+	}
+	/**
+	 * @return city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city セットする city
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return town
+	 */
+	public String getTown() {
+		return town;
+	}
+	/**
+	 * @param town セットする town
+	 */
+	public void setTown(String town) {
+		this.town = town;
+	}
+	/**
+	 * @return note
+	 */
+	public String getNote() {
+		return note;
+	}
+	/**
+	 * @param note セットする note
+	 */
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+}
