5cc715b1d4011b0ac32351c6fd98b68f35f45cfc
Y <y.hitoshi0104@gmail.com>
2017-12-07 13:25:10 +0900

    新規作成

---
+++ src/service/lead/update/AccountService.java
@@ -0,0 +1,21 @@
+package service.lead.update;
+
+import service.lead.dto.LeadDto;
+import util.session.SessionInfo;
+
+/**
+ * 見込み客更新のサービスクラス
+ *
+ */
+public class AccountService {
+
+	/**
+	 * 見込み客から取引先と取引先担当者を作成
+	 * @param si
+	 * @param dto
+	 */
+	public void createAccountAndContact(SessionInfo si, LeadDto dto) {
+
+	}
+
+}
