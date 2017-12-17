9adc6204b5738a7f933dcf022dfe1caddd59e994
Y <y.hitoshi0104@gmail.com>
2017-12-17 14:32:02 +0900

    JSONフォーマットの変更に対応

--- WebContent/js/lead/list.js
+++ WebContent/js/lead/list.js
@@ -38,7 +38,8 @@ $(function() {
 			}
 
 			var count = 0;
-			$(result).each( function () {
+			var data = result[0].data;
+			$(data).each( function () {
 
 				// テーブルに行を1行追加
 				$('.leadlistbody').append('<tr id="row' + count +'"></tr>');
@@ -46,19 +47,19 @@ $(function() {
 				// 行にカラムの設定
 				var id = '#row' + count;
 				// ID
-				$(id).append('<td class="idcolumn"><a href="/SFA/lead.update.show.action?leadid=' + result[count].LeadId + '">' + result[count].LeadId + '</a></td>');
+				$(id).append('<td class="idcolumn"><a href="/SFA/lead.update.show.action?leadid=' + data[count].LeadId + '">' + data[count].LeadId + '</a></td>');
 				// 姓
-				$(id).append('<td class="lastnamecolumn">' + moldingListItem(result[count].LastName, 12) + '</td>');
+				$(id).append('<td class="lastnamecolumn">' + moldingListItem(data[count].LastName, 12) + '</td>');
 				// 名
-				$(id).append('<td class="firstnamecolumn">' + moldingListItem(result[count].FirstName, 12) + '</td>');
+				$(id).append('<td class="firstnamecolumn">' + moldingListItem(data[count].FirstName, 12) + '</td>');
 				// 会社名
-				$(id).append('<td class="companynamecolumn">' + moldingListItem(result[count].CompanyName, 17) + '</td>');
+				$(id).append('<td class="companynamecolumn">' + moldingListItem(data[count].CompanyName, 17) + '</td>');
 				// 状況名称
-				$(id).append('<td class="statusnamecolumn">' + result[count].StatusName + '</td>');
+				$(id).append('<td class="statusnamecolumn">' + data[count].StatusName + '</td>');
 				// 評価名称
-				$(id).append('<td class="estimationnamecolumn">' + result[count].EstimationName + '</td>');
+				$(id).append('<td class="estimationnamecolumn">' + data[count].EstimationName + '</td>');
 				// 都道府県名称
-				$(id).append('<td class="divisionnamecolumn">' + result[count].DivisionName + '</td>');
+				$(id).append('<td class="divisionnamecolumn">' + data[count].DivisionName + '</td>');
 
 				count++;
 			});
