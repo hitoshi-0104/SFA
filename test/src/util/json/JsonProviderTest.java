4cc6561ef0a490d6c9b2bddc00e9b1b2d028afd7
Y <y.hitoshi0104@gmail.com>
2017-12-18 21:24:20 +0900

    JsonProviderクラスの変更に対応

--- test/src/util/json/JsonProviderTest.java
+++ test/src/util/json/JsonProviderTest.java
@@ -14,7 +14,7 @@ class JsonProviderTest {
 	@Test
 	void testProvideMapOfStringString() {
 
-		List<Map<String, String>> list = setDate();
+		List<Map<String, Object>> list = setDate();
 
 		String json = JsonProvider.provide(list.get(0));
 		assertEquals("{\"a\" : \"あいうえお\", \"b\" : \"かきくけこ\", \"c\" : \"さしすせそ\"}", json);
@@ -23,7 +23,7 @@ class JsonProviderTest {
 	@Test
 	void testProvideListOfMapOfStringString() {
 
-		List<Map<String, String>> list = setDate();
+		List<Map<String, Object>> list = setDate();
 
 		String json = JsonProvider.provide(list);
 		assertEquals("[{\"a\" : \"あいうえお\", \"b\" : \"かきくけこ\", \"c\" : \"さしすせそ\"},{\"d\" : \"たちつてと\", \"e\" : \"なにぬねの\", \"f\" : \"はひふへほ\"},{\"g\" : \"まみむめも\"}]", json);
@@ -33,23 +33,23 @@ class JsonProviderTest {
 	 * テストデータセット
 	 * @return
 	 */
-	private List<Map<String, String>> setDate() {
+	private List<Map<String, Object>> setDate() {
 
-		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
-		Map<String, String> m = new LinkedHashMap<String, String>();
+		Map<String, Object> m = new LinkedHashMap<String, Object>();
 		m.put("a", "あいうえお");
 		m.put("b", "かきくけこ");
 		m.put("c", "さしすせそ");
 		list.add(m);
 
-		m = new LinkedHashMap<String, String>();
+		m = new LinkedHashMap<String, Object>();
 		m.put("d", "たちつてと");
 		m.put("e", "なにぬねの");
 		m.put("f", "はひふへほ");
 		list.add(m);
 
-		m = new LinkedHashMap<String, String>();
+		m = new LinkedHashMap<String, Object>();
 		m.put("g", "まみむめも");
 		list.add(m);
 
