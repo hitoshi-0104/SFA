b732d3b19fb3565b9c3c3b5b0f3fdd25ed762753
Y <y.hitoshi0104@gmail.com>
2018-01-15 12:23:10 +0900

    新規追加

---
+++ WebContent/js/contact/list.js
@@ -0,0 +1,99 @@
+/**
+ * 取引先担当者一覧のJS
+ */
+$(function() {
+
+	// 検索ボタン押下時
+	$('#searchbutton').click(function() {
+
+		search(1);
+
+	});
+
+});
+
+// 一覧検索処理
+function search(curPage) {
+
+	$.ajax({
+		url: 'SFA/contact.dialog.search.rest',
+		type: 'POST',
+		data: {
+			'lastname' : $('input[name=lastname]').val(),
+			'firstname' : $('input[name=firstname]').val(),
+			'accountid' : $('input[name=accountid]').val(),
+			'page' : curPage
+		},
+		timeout: 10000,
+		// 送信前処理
+		beforeSend: function(xhr, settings) {
+			$('#searchbutton').attr('disabled', true);
+		}
+	}).done(function(result, textStatus, xhr) {
+
+		// 一覧を非表示
+		$('#contactlist').css('display', 'none');
+		$('#contactlistpagination').css('display', 'none');
+
+		// テーブルのクリア
+		$('#contactlistbody').empty();
+		$('#contactlistpagination').empty();
+
+		if (result == null || Object.keys(result).length === 0 || result[0].cnt === '0') {
+			return;
+		}
+
+		var cnt = Number(result[0].cnt);
+		var lastPage = cnt % Number(result[0].maxrow) == 0 ? Math.floor(cnt / Number(result[0].maxrow)) : Math.floor(cnt / Number(result[0].maxrow)) + 1;
+
+		// ページネーションの設定
+		if (lastPage > 1) {
+			$('#contactlistpagination').append('<ul id="pageul" class="pagination"></ul>');
+			var previousPage = curPage - 1 > 0 ? curPage - 1 : 1;
+			$('#pageul').append('<li class="page-item ' + (curPage == 1 ? 'disabled' : '') +  '"><button type="button" id="previousbutton" class="page-link" onclick="contactSearch('+ previousPage +')">前へ</button></li>');
+			var startIndex = curPage - 5 <= 0 ? 0 : curPage - 5;
+			var endIndex = lastPage < startIndex + 10 ? lastPage : startIndex + 10;
+			startIndex = endIndex - 10;
+			if (startIndex < 0) {
+				startIndex = 0;
+			}
+			for (var i = startIndex; i < endIndex; i++) {
+				var buttonId = 'page' + (i + 1);
+				$('#pageul').append('<li class="page-item ' + (i == (curPage - 1) ? 'active' : '') + '"><button type="button" id="' + buttonId + '" class="page-link" onclick="contactSearch(' + (i + 1) + ')">' + (i + 1) +'</button></li>');
+			}
+			$('#pageul').append('<li class="page-item ' + (curPage == lastPage ? 'disabled' : '') + '"><button type="button" id="nextbutton" class="page-link" onclick="contactSearch(' + (curPage + 1) + ')">次へ</button></li>');
+		}
+
+		var count = 0;
+		var data = result[0].data;
+		$(data).each( function () {
+
+			// テーブルに行を1行追加
+			$('#contactlistbody').append('<tr id="row' + count +'"></tr>');
+
+			// 行にカラムの設定
+			var id = '#row' + count;
+			// ID
+			$(id).append('<td id="col' + count + '1" class="contactidcolumn">' + data[count].ContactId + '</td>');
+			// 取引先担当者名
+			$(id).append('<td id="col' + count + '2" class="contactnamecolumn">' + moldingListItem(data[count].ContactName, 15) + '</td>');
+			// 取引先
+			$(id).append('<td id="col' + count + '3" class="accountcolumn">' + moldingListItem(data[count].AccountName, 13) + '</td>');
+			// 携帯
+			$(id).append('<td id="col' + count + '4" class="mobilephonecolumn">' + moldingListItem(data[count].MibolePhone, 13) + '</td>');
+			// メール
+			$(id).append('<td id="col' + count + '5" class="mailcolumn">' + moldingListItem(data[count].Mail, 13) + '</td>');
+
+			count++;
+		});
+
+		// 一覧を表示
+		$('#contactlist').css('display', 'inline');
+		$('#contactlistpagination').css('display', 'inline');
+
+	}).fail(function(xhr, textStatus, error) {
+		window.alert(error);
+	}).always(function(xhr, settings) {
+		$('#contactsearchbutton').attr('disabled', false);
+	});
+}
