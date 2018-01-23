e882c169a981b7f60c07602a24998d6c98b9ed01
Y <y.hitoshi0104@gmail.com>
2018-01-23 12:45:55 +0900

    Update用パラメータを追加

--- src/action/contact/constant/ReqParam.java
+++ src/action/contact/constant/ReqParam.java
@@ -38,6 +38,37 @@ public class ReqParam {
 	}
 
 	/**
+	 * 更新
+	 *
+	 */
+	public class Update {
+		/** 取引先担当者ID */
+		public static final String ID = "id";
+		/** 姓 */
+		public static final String LAST_NAME = "lastname";
+		/** 名 */
+		public static final String FIRST_NAME = "firstname";
+		/** 取引先ID */
+		public static final String ACCOUNT_ID = "accountid";
+		/** 部署 */
+		public static final String DEPARTMENT = "department";
+		/** 役職 */
+		public static final String POSITION = "position";
+		/** 電話 */
+		public static final String PHONE = "phone";
+		/** 携帯 */
+		public static final String MOBILE_PHONE = "mobilephone";
+		/** FAX */
+		public static final String FAX = "fax";
+		/** メール */
+		public static final String MAIL = "mail";
+		/** 上司 */
+		public static final String BOSS = "boss";
+		/** その他 */
+		public static final String NOTE = "note";
+	}
+
+	/**
 	 * 一覧
 	 *
 	 */
