d722e67fe6523d6fff0286b1e0fe87cf7cb4a081
Y <y.hitoshi0104@gmail.com>
2018-01-23 12:27:40 +0900

    取得データをDTOに移す処理の実装

--- src/service/contact/update/SearchService.java
+++ src/service/contact/update/SearchService.java
@@ -26,13 +26,43 @@ public class SearchService {
 		// データ取得
 		ConnectionProvider cp = ConnectionProvider.getInstance();
 		ContactDao dao = new ContactDao(cp);
-		ContactEntity entity = null;
+		ContactEntity en = null;
 		try(Connection conn = cp.getConnection()) {
-			entity = dao.selectById(dto.getId());
+			en = dao.selectById(dto.getId());
 		}
 
 		// DTO作成
 		UpdateDto ret = new UpdateDto();
+
+		// 取引先担当者ID
+		ret.setId(en.getId());
+		// 姓
+		ret.setLastName(en.getLastName());
+		// 名
+		ret.setFirstName(en.getFirstName());
+		// 取引先
+		ret.setAccountId(en.getAccountId());
+		// 取引先名
+		ret.setAccountName(en.getAccountName());
+		// 部署
+		ret.setDepartment(en.getDepartment());
+		// 役職
+		ret.setPosition(en.getPosition());
+		// 電話
+		ret.setPhone(en.getPhone());
+		// 携帯
+		ret.setMobilePhone(en.getMobilePhone());
+		// FAX
+		ret.setFax(en.getFax());
+		// メール
+		ret.setMail(en.getMail());
+		// 上司
+		ret.setBoss(en.getBoss());
+		// 上司名
+		ret.setBossName(en.getBossName());
+		// その他
+		ret.setNote(en.getNote());
+
 		return ret;
 	}
 
